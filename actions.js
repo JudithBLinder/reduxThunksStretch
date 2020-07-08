f9552bee7a7df308035a1a59d8362df4f9fdb58f
JudithBLinder <judithrooz@gmail.com>
2020-07-08 18:57:24 -0400

    not done

--- actions.js
+++ actions.js
@@ -3,5 +3,17 @@ const axios = require('axios');
 //TYPES
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
-/* YOUR CODE GOES HERE */
+const gotDesserts = (obj) => ({ type: GOT_DESSERTS_DATA, desserts: obj });
+const addDessert = (obj) => ({ type: NEW_DESSERT_DATA, newDessert: obj });
+const fetchDesserts = () => {
+  const desserts = axios.get('/api/desserts');
+  return function (dispatch) {
+    dispatch(gotDesserts(desserts.data));
+  };
+};
+const postDessert = () => {
+  return function (dispatch) {
+    dispatch(addDessert());
+  };
+};
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
